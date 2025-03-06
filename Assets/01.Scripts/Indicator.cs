8b046a89c011ee07a01234fac948d1d9b8dbdfd5
briankunkel9 <briankunkel9@gmail.com>
2025-03-06 22:45:43 +0900

    add indicator ui

---
+++ Assets/01.Scripts/Indicator.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class Indicator : MonoBehaviour
+{
+    public Transform target;
+    public RectTransform ui;
+    public RectTransform tail;
+    public CanvasScaler scaler;
+    public float radius = 1.0f;
+
+    private bool IsOffScreen()
+    {
+        Vector2 vec = Camera.main.WorldToViewportPoint(target.position);
+        if (vec.x < 0 || vec.x > 1 || vec.y <= 0 || vec.y > 1) return true;
+        return false;
+    }
+
+    private void Update()
+    {
+        var position = Camera.main.WorldToScreenPoint(target.position);
+       
+        Vector2 min = new Vector2(ui.rect.width * 0.5f, ui.rect.height * 0.5f);
+        Vector2 screenSize = Camera.main.ViewportToScreenPoint(Vector2.one);
+        Vector2 max = new Vector2(screenSize.x - min.x, screenSize.y - min.y);
+
+        position.x = Mathf.Clamp(position.x, min.x, screenSize.x - min.x);
+        position.y = Mathf.Clamp(position.y, min.y, screenSize.y - min.y);
+
+        Vector3 offset = Vector3.zero;
+        if(IsOffScreen())
+        {
+            Vector2 dir = target.position - Camera.main.ViewportToWorldPoint(Vector3.one * 0.5f);
+            dir = dir.normalized;
+            tail.right = dir;
+            offset = dir * radius;
+        }
+        else
+        {
+            tail.right = Vector2.down;
+            offset = Vector3.down * 0f;
+        }
+        ui.position = position;
+        tail.position = position + offset;
+    }
+}
